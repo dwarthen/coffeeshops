8666ec914f9799913e1a51cadc150226b6136bfe
David Warthen <david@davidwarthen.com>
2018-07-15 22:53:41 -0700

    added access control headers

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ const googleMapsClient = require('@google/maps').createClient({
 
 app.use(express.json());
 
+//access control headers
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 //local modules
 const cs = require('./coffee_shops');
 
